b9a143d838d687a411cfb83679f27d7f92e8a48d
Markus Hedvall <mackanhedvall@gmail.com>
2015-07-30 07:11:25 +0200

    Replace coffeescript with javascript
    All cson have been replaced with json and all coffee files has
    been rewritten into javascript following standard style.

--- spec/plantuml-preview-view-spec.coffee
+++
@@ -1,5 +0,0 @@
-PlantumlPreviewView = require '../lib/plantuml-preview-view'
-
-describe "PlantumlPreviewView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
---
+++ spec/plantuml-preview-view-spec.js
@@ -0,0 +1,8 @@
+/* global describe it expect */
+'use strict'
+
+describe('PlantumlPreviewView', function () {
+  return it('has one valid test', function () {
+    return expect('life').toBe('easy')
+  })
+})
