a384b48dbdc9768519c0efa230fd7af8936ba0b9
Markus Hedvall <mackanhedvall@gmail.com>
2015-07-30 22:50:04 +0200

    Add prevention of toggling when not editing plantuml
    When toggling there is a check to see that the active editor
    uses plantuml grammar. This depends on language-plantuml being
    installed.

--- spec/plantuml-preview-spec.js
+++ spec/plantuml-preview-spec.js
@@ -1,29 +1,51 @@
 /* global atom jasmine beforeEach waitsForPromise runs describe it expect */
 'use strict'
 
+var temp = require('temp')
+var path = require('path')
+var wrench = require('wrench')
+
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
+
+var FIXTURES_PATH = path.join(__dirname, 'fixtures')
+
+temp.track()
 
 describe('PlantumlPreview', function () {
   var activationPromise
   var workspaceElement
 
   beforeEach(function () {
+    var tempPath = temp.mkdirSync('plantuml-preview')
+    wrench.copyDirSyncRecursive(FIXTURES_PATH, tempPath, { forceDelete: true })
+    atom.project.setPaths([tempPath])
+
     workspaceElement = atom.views.getView(atom.workspace)
     activationPromise = atom.packages.activatePackage('plantuml-preview')
+
+    waitsForPromise(function () {
+      return atom.packages.activatePackage('language-plantuml')
+    })
   })
 
   describe('when the plantuml-preview:toggle event is triggered', function () {
     it('hides and shows the modal panel', function () {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.plantuml-preview')).not.toExist()
+      waitsForPromise(function () {
+        return atom.workspace.open('file.puml')
+      })
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'plantuml-preview:toggle')
+      runs(function () {
+        // Before the activation event the view is not on the DOM, and no panel
+        // has been created
+        expect(workspaceElement.querySelector('.plantuml-preview')).not.toExist()
+
+        // This is an activation event, triggering it will cause the package to be
+        // activated.
+        atom.commands.dispatch(workspaceElement, 'plantuml-preview:toggle')
+      })
 
       waitsForPromise(function () {
         return activationPromise
@@ -45,17 +67,23 @@ describe('PlantumlPreview', function () {
     it('hides and shows the view', function () {
       // This test shows you an integration test testing at the view level.
 
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement)
+      waitsForPromise(function () {
+        return atom.workspace.open('file.puml')
+      })
 
-      expect(workspaceElement.querySelector('.plantuml-preview')).not.toExist()
+      runs(function () {
+        // Attaching the workspaceElement to the DOM is required to allow the
+        // `toBeVisible()` matchers to work. Anything testing visibility or focus
+        // requires that the workspaceElement is on the DOM. Tests that attach the
+        // workspaceElement to the DOM are generally slower than those off DOM.
+        jasmine.attachToDOM(workspaceElement)
 
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'plantuml-preview:toggle')
+        expect(workspaceElement.querySelector('.plantuml-preview')).not.toExist()
+
+        // This is an activation event, triggering it causes the package to be
+        // activated.
+        atom.commands.dispatch(workspaceElement, 'plantuml-preview:toggle')
+      })
 
       waitsForPromise(function () {
         return activationPromise
