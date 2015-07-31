748f8344ac745084505793131b24736334034d2b
Markus Hedvall <mackanhedvall@gmail.com>
2015-07-31 20:00:35 +0200

    Clean up some code
    Unnecessary comments has been removed from both the package code
    and test code. Also some minor fixes to simplify the code.

--- spec/plantuml-preview-spec.js
+++ spec/plantuml-preview-spec.js
@@ -5,11 +5,6 @@ var temp = require('temp')
 var path = require('path')
 var wrench = require('wrench')
 
-// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
-
 var FIXTURES_PATH = path.join(__dirname, 'fixtures')
 
 temp.track()
@@ -38,12 +33,7 @@ describe('PlantumlPreview', function () {
       })
 
       runs(function () {
-        // Before the activation event the view is not on the DOM, and no panel
-        // has been created
         expect(workspaceElement.querySelector('.plantuml-preview')).not.toExist()
-
-        // This is an activation event, triggering it will cause the package to be
-        // activated.
         atom.commands.dispatch(workspaceElement, 'plantuml-preview:toggle')
       })
 
@@ -65,23 +55,13 @@ describe('PlantumlPreview', function () {
     })
 
     it('hides and shows the view', function () {
-      // This test shows you an integration test testing at the view level.
-
       waitsForPromise(function () {
         return atom.workspace.open('file.puml')
       })
 
       runs(function () {
-        // Attaching the workspaceElement to the DOM is required to allow the
-        // `toBeVisible()` matchers to work. Anything testing visibility or focus
-        // requires that the workspaceElement is on the DOM. Tests that attach the
-        // workspaceElement to the DOM are generally slower than those off DOM.
         jasmine.attachToDOM(workspaceElement)
-
         expect(workspaceElement.querySelector('.plantuml-preview')).not.toExist()
-
-        // This is an activation event, triggering it causes the package to be
-        // activated.
         atom.commands.dispatch(workspaceElement, 'plantuml-preview:toggle')
       })
 
@@ -90,7 +70,6 @@ describe('PlantumlPreview', function () {
       })
 
       runs(function () {
-        // Now we can test for view visibility
         var plantumlPreviewElement = workspaceElement.querySelector('.plantuml-preview')
         expect(plantumlPreviewElement).toBeVisible()
         atom.commands.dispatch(workspaceElement, 'plantuml-preview:toggle')
