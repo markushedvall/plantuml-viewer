44b0692ab0cba42114f1200fe346ea8a6e1c1957
Markus Hedvall <mackanhedvall@gmail.com>
2015-08-02 13:16:58 +0200

    Add mirroring of plantuml editor to preview editor
    The preview editor mimics the plantuml editor by returning the
    same text and path. The titlse is the same but with a Preview
    suffix. The returned URI is the unique plantuml-preview uri.

--- spec/plantuml-preview-editor-spec.js
+++ spec/plantuml-preview-editor-spec.js
@@ -1,16 +1,85 @@
-/* global describe it expect */
+/* global atom beforeEach waitsForPromise runs describe it expect */
 'use strict'
 
 var PlantumlPreviewEditor = require('../lib/plantuml-preview-editor')
 var PlantumlPreviewView = require('../lib/plantuml-preview-view')
+var temp = require('temp')
+var path = require('path')
+var wrench = require('wrench')
+
+var FIXTURES_PATH = path.join(__dirname, 'fixtures')
+
+temp.track()
 
 describe('PlantumlPreviewEditor', function () {
+
+  beforeEach(function () {
+    var tempPath = temp.mkdirSync('plantuml-preview')
+    wrench.copyDirSyncRecursive(FIXTURES_PATH, tempPath, { forceDelete: true })
+    atom.project.setPaths([tempPath])
+  })
+
+  function waitsForOpeningEditor () {
+    waitsForPromise(function () {
+      return atom.workspace.open('file.puml')
+    })
+  }
+
   describe('::getViewClass', function () {
-    it('returns constructor for PlantumlPreviewView', function () {
-      var editor = new PlantumlPreviewEditor(42)
-      var ViewCLass = editor.getViewClass()
-      var view = new ViewCLass()
-      expect(view).toBeInstanceOf(PlantumlPreviewView)
+    it('should be a constructor for PlantumlPreviewView', function () {
+      waitsForOpeningEditor()
+      runs(function () {
+        var editor = atom.workspace.getActiveTextEditor()
+        var previewEditor = new PlantumlPreviewEditor('uri', editor.id)
+        var ViewCLass = previewEditor.getViewClass()
+        expect(ViewCLass).toBe(PlantumlPreviewView)
+      })
+    })
+  })
+
+  describe('::getUri', function () {
+    it('should be same uri as put into the constructor', function () {
+      waitsForOpeningEditor()
+      runs(function () {
+        var editor = atom.workspace.getActiveTextEditor()
+
+        var uri = 'uri'
+        var previewEditor = new PlantumlPreviewEditor(uri, editor.id)
+        expect(previewEditor.getURI()).toBe(uri)
+      })
+    })
+  })
+
+  describe('::getTitle', function () {
+    it('should be plantuml editor title + " Preview"', function () {
+      waitsForOpeningEditor()
+      runs(function () {
+        var editor = atom.workspace.getActiveTextEditor()
+        var previewEditor = new PlantumlPreviewEditor('uri', editor.id)
+        expect(previewEditor.getTitle()).toBe(editor.getTitle() + ' Preview')
+      })
+    })
+  })
+
+  describe('::getPath', function () {
+    it('should be same as plantuml editor path', function () {
+      waitsForOpeningEditor()
+      runs(function () {
+        var editor = atom.workspace.getActiveTextEditor()
+        var previewEditor = new PlantumlPreviewEditor('uri', editor.id)
+        expect(previewEditor.getPath()).toBe(editor.getPath())
+      })
+    })
+  })
+
+  describe('::getText', function () {
+    it('should be same as plantuml editor text', function () {
+      waitsForOpeningEditor()
+      runs(function () {
+        var editor = atom.workspace.getActiveTextEditor()
+        var previewEditor = new PlantumlPreviewEditor('uri', editor.id)
+        expect(previewEditor.getText()).toBe(editor.getText())
+      })
     })
   })
 })
