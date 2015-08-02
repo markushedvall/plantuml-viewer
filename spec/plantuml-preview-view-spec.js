19a738a7ea5edd193f536795c509e49699bc6dbb
Markus Hedvall <mackanhedvall@gmail.com>
2015-08-02 18:50:48 +0200

    Make the view display an plantuml image
    The view has been updated to take the text from the editor and
    generate a image to a temporary file. The temporary file is then
    displayed.

--- spec/plantuml-preview-view-spec.js
+++ spec/plantuml-preview-view-spec.js
@@ -1,8 +1,46 @@
-/* global describe it expect */
+/* global atom beforeEach waitsForPromise waitsFor runs describe it expect */
 'use strict'
 
+var PlantumlPreviewEditor = require('../lib/plantuml-preview-editor')
+var PlantumlPreviewView = require('../lib/plantuml-preview-view')
+var plantuml = require('node-plantuml')
+var temp = require('temp')
+var fs = require('fs')
+
 describe('PlantumlPreviewView', function () {
-  return it('has one valid test', function () {
-    return expect('life').toBe('life')
+  var editor
+  var view
+
+  beforeEach(function () {
+    waitsForPromise(function () {
+      return atom.workspace.open('file.puml')
+    })
+    runs(function () {
+      editor = atom.workspace.getActiveTextEditor()
+      var previewEditor = new PlantumlPreviewEditor('uri', editor.id)
+      view = new PlantumlPreviewView(previewEditor)
+    })
+    waitsFor(function () {
+      return view.image.attr('src')
+    })
+  })
+
+  return it('should contain png generated from text editor', function () {
+    var tempStream = temp.createWriteStream()
+    var gen = plantuml.generate(editor.getText(), { format: 'png' })
+    gen.out.pipe(tempStream)
+
+    waitsFor(function (done) {
+      tempStream.on('finish', done)
+    })
+
+    runs(function () {
+      var previewFilePath = view.image.attr('src')
+      var previewImage = fs.readFileSync(previewFilePath)
+      var expectedImage = fs.readFileSync(tempStream.path)
+
+      var diff = Buffer.compare(previewImage, expectedImage)
+      expect(diff).toBe(0)
+    })
   })
 })
